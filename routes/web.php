bc2dcb37ac3e82016fb03abcd30be2914db6b990
Saurav mishra <mishrasaurav499@gmail.com>
2026-04-21 14:12:30 +0530

    Named Route definations and using controllers in routes

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/home', function() {
+//     return ('hello form webserver articen');
+// });
+
+Route::redirect('/home', '/');
+
+Route::get('/product/{productName}', function ($productName) {
+    return view('product', ['Name' => $productName]);
+});
+
+Route::get('/payment', function () {
+    return 'this is the paymment page';
+});
+
+Route::get('/cart', function () {
+    return 'this is cart page';
+});
+
+Route::get('/products/{name}', [ProductController::class, 'getProduct']);
+
+Route::view('/signup', 'signup');
+
+Route::view('/userform', 'user-form');
+
+Route::post('/adduser', [FormController::class, 'addUser'])->name('adduser');
